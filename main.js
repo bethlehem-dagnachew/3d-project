309962a031ba719dbfafa2c363ab69112aef8ff7
bethlehem-dagnachew <52574965+bethlehem-dagnachew@users.noreply.github.com>
2024-09-13 15:54:41 +0300

    add animation, postion, size

--- main.js
+++ main.js
@@ -1,28 +1,104 @@
 import * as THREE from "three";
+import gsap from "gsap";
+import "/style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector(".webgl"),
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+  roughness: 0.4,
 });
-renderer.setSize(window.innerWidth, window.innerHeight);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+//sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+//light
+const light = new THREE.PointLight(0xffffff, 3, 200);
+light.position.set(1, 5, 5);
+scene.add(light);
+
+//camera
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 10;
+scene.add(camera);
+
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+renderer.setPixelRatio(2);
+
+//resize
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
+
+//controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+const loop = () => {
+  controls.update();
   renderer.render(scene, camera);
-}
-animate();
+  window.requestAnimationFrame(loop);
+};
+loop();
+
+//timeline
+const t1 = gsap.timeline({
+  defaults: { duration: 1 },
+});
+t1.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+t1.fromTo("nav", { y: "-100%" }, { y: "0%" });
+
+//mouse anima color
+
+let mouseDown = false;
+let rgb = [];
+
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pagey / sizes.height) * 255),
+      150,
+    ];
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
